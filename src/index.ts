c02657da0abe499cbfbd105c55320a3e5b223ce7
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-10-01 03:15:09 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+function countWords(text: string): number {
+  const words = text.split(/\s+/).filter(word => word !== '');
+  return words.length;
+}
+console.log(chalk.cyan(`*********** Count Words App ****************`))
+async function main() {
+  const questions = [
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter the text you want to count words in:',
+    },
+  ];
+
+  const answers = await inquirer.prompt(questions);
+  const { text } = answers;
+
+  const wordCount = countWords(text);
+
+  console.log(chalk.green(`Word count: ${wordCount}`));
+}
+
+main();
